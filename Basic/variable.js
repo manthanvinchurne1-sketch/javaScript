947959c4ed7a71f5a7dd45054c3ef267be6ffabd
Manthan Rakesh Vinchurne <manthanvinchurne1@gmail.com>
2026-06-13 17:26:31 +0000

    variables

---
+++ Basic/variable.js
@@ -0,0 +1,7 @@
+const accountId = 31102008 // the const key word is used to make a variable constant 
+let accountEmail = "manthan@google.com" //the let is used to describe the vaiable 
+var accountPassword = 3957 // the var is also used to describe the variable but the problem with var is it can not define the scope of the code
+accountCity = "Nagpur" // in js you can also declare the variable without using let or var
+let accountState; // in this case you will recive a null value or undefined 
+
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])

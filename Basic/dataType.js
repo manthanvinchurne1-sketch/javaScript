c3c4aaec8b2223fd0cfaa30edecd920932681644
Manthan Rakesh Vinchurne <manthanvinchurne1@gmail.com>
2026-06-13 17:26:05 +0000

    dataTypes

---
+++ Basic/dataType.js
@@ -0,0 +1,23 @@
+let name = "Manthan" // string data type 
+let age = 20 // integr data type
+let logIn = true // boolean data type 
+
+/* types of data type
+ 1 integer -> scope of integer is 2 to the power 53
+ 2 bigInt -> used for large numbers 
+ 3 string -> used in '' or "" 
+ 4 Boolean -> true or false 
+ 5 null -> it is a stand alone value 
+ 6 undefined -> the value which is not defined  
+ 7 symbol -> defined for qniqueness of variable 
+ */
+
+ /*
+  1 object ->
+  2 typeof -> to define the type of any variable 
+ */
+
+   console.log(typeof null); // object 
+   console.log(typeof undefined); // undefined
+   
+   

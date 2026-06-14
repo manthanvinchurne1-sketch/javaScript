c616c522913c1175638e65273b62ab09af72b2a4
Manthan Rakesh Vinchurne <manthanvinchurne1@gmail.com>
2026-06-14 06:18:27 +0000

    basic operation

---
+++ Basic/operation.js
@@ -0,0 +1,42 @@
+let str1 = "Hello"
+let str2 = " Manthan"
+let str3 = str1 + str2 
+console.log(str3);  // adds the two string with the help of "+" operator NOTE -> can note subtract the string
+
+/* BASIC OPERATORS 
+
+1 + -> addition operator 
+2 - -> subtraction operator 
+3 * -> multiplication operator
+4 / -> division operator 
+5 ** -> power operator 
+6 % -> return reminder operator 
+
+*/
+
+let num = 3 
+let value = -num
+console.log(value);  // returns the negitive value because of "-" operator
+
+console.log("1"+2); // returns 12 beacause the first number is string so 1 calculate as string and 2 is addded in string 
+console.log(1+"2"); // in this case we also get 12 beacause 1 is the integer type and 2 is string type so 1 is added in string 
+console.log("1"+2+2); // it returns 122 beacause at the srating we have string so it changes the whole addition into string addition 
+console.log(1+2+"2"); // it returns 32 because the string is at last position in array so at the starting the normal 
+
+
+let gameCounter = 100
+console.log(gameCounter);
+
+++gameCounter              // pre increment is used to increment the value of  variable before it calls 
+console.log(gameCounter)
+
+let counter = 99
+console.log(counter);
+
+counter ++             // post increment is used to increment the value of variable after it calls
+console.log(counter);
+
+
+
+
+

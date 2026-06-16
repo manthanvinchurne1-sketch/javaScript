8da947e22b22f5e5b39462af3a5c35bd6483a0c1
Manthan Rakesh Vinchurne <manthanvinchurne1@gmail.com>
2026-06-16 05:02:53 +0000

    comparison of  values

---
+++ Basic/comparison.js
@@ -0,0 +1,38 @@
+// BASIC COMAPARISON OPERATIONS
+/*console.log(2>1); //true
+console.log(2<1); //false
+console.log(2==1); //false : it checks the value of the of the data not the data type
+console.log(2>=1); //true
+console.log(2<=1); //false
+console.log("3"===1); // false : in this case it cheks the data type of the value if it is diff returns false*/
+
+/* note : comparison operators return the values in boolean form */
+
+
+/*console.log("2">1);  // true 
+console.log("02">1); // true
+console.log("2">=1); // true*/
+
+/* note : java script automatically converts the string into number */
+
+/*console.log(null > 0); //false
+console.log(null == 0); //false
+console.log(null >= 0); //true
+console.log(null <= 0); //true */
+
+/* note : the comparison operators and equality operators works differently 
+         equality operators check the value of the if they are same the true otherwise false
+         and comparison operators treat null as value "0" thats why null >= 0 is true and
+         null > 0 is false */
+
+console.log(undefined == 0); //false
+console.log(undefined === 0); //false 
+console.log(undefined > 0); //false
+console.log(undefined < 0); //false
+
+
+/* note : because the undefined is the variabe returned when the value is not given and in the 
+          above cases the values are not given so far so it returns false */
+
+
+
